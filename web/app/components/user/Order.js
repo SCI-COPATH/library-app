482336f3f4c5e2a3477abc24e3235338356abecc
jayasankar J P <scicopath01@gmail.com>
2024-01-01 17:40:35 +0530

    add orders page in address

--- web/app/components/user/Order.js
+++ web/app/components/user/Order.js
@@ -1,20 +1,21 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import Page from "../Page"
 import Header from "../header/Header"
 import StateContext from "../../StateContext"
 
 function Order() {
   const appState = useContext(StateContext)
-  console.log(appState.order)
-  console.log(appState.items)
-  console.log("Start")
-  appState.order.map((oreder) => {
-    console.log(appState.items.filter((item) => item.id == oreder.itemId))
-  })
+
+  // console.log(appState.order)
+  // console.log(appState.items)
+  // console.log("Start")
+  // appState.order.map((oreder) => {
+  //   console.log(appState.items.filter((item) => item.id == oreder.itemId))
+  // })
   // appState.items.map((item) => {
   //   console.log(appState.order.filter((order) => order.itemId == item.id))
   // })
-  console.log("end")
+  // console.log("end")
   return (
     <>
       <Header />
@@ -22,10 +23,12 @@ function Order() {
         <h3>Orders</h3>
         <div className="row justify-content-between">
           {appState.order.map((order) => {
+            let address = appState.address.filter((add) => add.id == order.address_id)[0]
+
             return (
               <>
                 <div className="container m-4 " style={{ maxWidth: "max-content" }} key={order.id}>
-                  <div className="card border-0 rounded-0 shadow" style={{ width: "18rem", maxWidth: "18rem" }}>
+                  <div className="card border-0 rounded-0 shadow " style={{ width: "18rem", maxWidth: "18rem" }}>
                     <img src={`http://localhost:8080/images/items/${appState.items.filter((item) => item.id == order.itemId)[0].image}`} className="card-img-top rounded-0" />
                     <div className="card-body mt-3 mb-3">
                       <div className="row">
@@ -40,9 +43,18 @@ function Order() {
                           <div>
                             Status:<strong>{order.status}</strong>
                           </div>
-                        </div>
-                        <div className="col-2">
-                          <i className="bi bi-bookmark-plus fs-2"></i>
+                          <div>
+                            <div>
+                              <strong>Address</strong>
+                              <div>{address.name}</div>
+                              <div>{address.line1}</div>
+                              <div>{address.line2}</div>
+                              <div>
+                                <span>{address.City}</span> <span>{address.pincode}</span>
+                              </div>
+                              <div>{address.phone_number}</div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
